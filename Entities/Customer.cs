4ec0bcb6b1adb546fa8452d7255dd175d87807d1
PauloFeijo <paulorikrdo.89@gmail.com>
2021-10-01 14:07:29 -0300

    Simple example mongoDb CRUD

---
+++ Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace MongoDbSimple.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using MongoDB.Driver;
+using MongoDbSimple.Entities;
+using System;
+
+namespace MongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Paul",
+                Address = "Street blue bird, 457",
+                Phone = "1234567489"
+            };
+
+            // Create
+            SaveCustomerOnMongoDb(customer);
+
+            // Get
+            var customer2 = GetCustomerFromMongoDb(customer.Id);
+
+            // Update
+            customer2.Name = "Joe";
+            UpdateCustomerOnMongoDb(customer2);
+
+            // Delete
+            DeleteCustomerOnMongoDb(customer2);
+        }
+
+        private static IMongoCollection<Customer> CreateCollecion()
+        {
+            var connectionString = "mongodb://root:root@localhost:27017";
+            var databaseName = "local";
+            var mongoCLient = new MongoClient(connectionString);
+            var dataBase = mongoCLient.GetDatabase(databaseName);
+            return dataBase.GetCollection<Customer>(nameof(Customer));
+        }
+
+        private static void SaveCustomerOnMongoDb(Customer customer)
+        {
+            CreateCollecion().InsertOne(customer);
+            Console.WriteLine($"Customer {customer.Name} inserted on mongoDb");
+        }
+
+        private static void UpdateCustomerOnMongoDb(Customer customer)
+        {
+            CreateCollecion().ReplaceOne(x => x.Id == customer.Id, customer);
+            Console.WriteLine($"Customer {customer.Name} updated on mongoDb");
+        }
+
+        private static Customer GetCustomerFromMongoDb(Guid id)
+        {
+            var customer = CreateCollecion().Find(x => x.Id == id).FirstOrDefault();
+            Console.WriteLine($"Customer {customer.Name} getted from mongoDb");
+            return customer;
+        }
+
+        private static void DeleteCustomerOnMongoDb(Customer customer)
+        {
+            CreateCollecion().DeleteOne(x => x.Id == customer.Id);
+            Console.WriteLine($"Customer {customer.Name} deleted from mongoDb");
+        }
+    }
+}
